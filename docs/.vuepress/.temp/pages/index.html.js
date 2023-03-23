e0a658af24e2fd1e479ef6b83600ef5c23c8f3c5
zxiaogong <1120197042@qq.com>
2023-03-23 20:10:29 +0800

    update: 初始化博客首页

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,1 +1,14 @@
-export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"个人博客首页\",\"slug\":\"个人博客首页\",\"link\":\"#个人博客首页\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"index.md\"}")
+export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{\"home\":true,\"heroText\":\"个人博客\",\"heroTitle\":\"个人博客\",\"tagline\":\"个人博客2\",\"features\":[{\"title\":\"简洁至上\",\"details\":\"以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。\"},{\"title\":\"Vue 驱动\",\"details\":\"享受 Vue 的开发体验，可以在 Markdown 中使用 Vue 组件，又可以使用 Vue 来开发自定义主题。\"},{\"title\":\"高性能\",\"details\":\"VuePress 会为每个页面预渲染生成静态的 HTML，同时，每个页面被加载的时候，将作为 SPA 运行。\"}]},\"headers\":[],\"git\":{\"updatedTime\":1679559743000,\"contributors\":[{\"name\":\"zxiaogong\",\"email\":\"1120197042@qq.com\",\"commits\":1}]},\"filePathRelative\":\"index.md\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
--- docs/.vuepress/.temp/pages/index.html.vue
+++ docs/.vuepress/.temp/pages/index.html.vue
@@ -1,4 +1,3 @@
-<template><div><h2 id="个人博客首页" tabindex="-1"><a class="header-anchor" href="#个人博客首页" aria-hidden="true">#</a> 个人博客首页</h2>
-</div></template>
+<template><div></div></template>
 
 
